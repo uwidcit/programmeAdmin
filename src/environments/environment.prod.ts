d10b92d0a6345f5ffb62009ac6abc6dc0913f367
Qarun <qarun80@hotmail.com>
2018-06-29 02:18:32 -0400

    - Fixed an issue with environment variables not being defined

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,19 @@
 export const environment = {
   production: true,
   facURL: 'https://snickdx.me:3004/sta/faculties',
-  deptsURL: `https://snickdx.me:3004/sta/faculties/{dept_name}/`,
-  allProgsByFac: `https://snickdx.me:3004/sta/programmes/{dept_name}`,
-  allProgsByDept: `https://snickdx.me:3004/sta/programmes/{fac_name}/{dept_name}`,
+  // deptsURL: 'https://snickdx.me:3004/sta/faculties/',
+  allProgsBy: 'https://snickdx.me:3004/sta/programmes/',
+  // allProgsByFac: `https://snickdx.me:3004/sta/programmes/{fac_name}`,
+  // allProgsByDept: `https://snickdx.me:3004/sta/programmes/{fac_name}/{dept_name}`,
   programmes: 'https://snickdx.me:3004/sta/programmes',
-  subjects: 'https://snickdx.me:3004/subjects'
+  subjects: 'https://snickdx.me:3004/subjects',
+  faculties: {
+    'Social Sciences': 'fss',
+    'Engineering': 'eng',
+    'Food & Agriculture': 'ffa',
+    'Humanities & Education': 'fhe',
+    'Medical Sciences': 'fms',
+    'Law': 'law',
+    'Science & Technology': 'fst'
+  },
 };
