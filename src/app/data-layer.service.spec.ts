ec63a64b44a7c6bb59486e8b8ea75a2d3c056c72
Qarun <qarun80@hotmail.com>
2018-06-29 02:13:02 -0400

    - Fixed an issue with HttpClientModule not being included in the data-layer.service.spec.ts

--- src/app/data-layer.service.spec.ts
+++ src/app/data-layer.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { DataLayerService } from './data-layer.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('DataLayerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [DataLayerService]
     });
   });
