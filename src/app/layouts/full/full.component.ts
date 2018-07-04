1b7084f125dd17f330097abce6094aedefeba4cb
Qarun <qarun80@hotmail.com>
2018-07-04 12:18:18 -0400

    Fixed an issue with side nav appearing on login screen

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -1,13 +1,16 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, NgZone, OnDestroy, ViewChild, HostListener, Directive, AfterViewInit } from '@angular/core';
 import { MenuItems } from '../../shared/menu-items/menu-items';
-import {Router} from '@angular/router';
+import {Router, NavigationEnd} from '@angular/router';
+import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
+
 
 /** @title Responsive sidenav */
 @Component({
   selector: 'app-full-layout',
   templateUrl: 'full.component.html',
   styleUrls: [],
+  providers: [MatDrawer],
 })
 export class FullComponent implements OnDestroy, AfterViewInit {
   mobileQuery: MediaQueryList;
@@ -15,13 +18,20 @@ export class FullComponent implements OnDestroy, AfterViewInit {
 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, public menuItems: MenuItems, public router: Router) {
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    public menuItems: MenuItems,
+    public router: Router,
+    public sidenav: MatDrawer) {
     this.mobileQuery = media.matchMedia('(min-width: 768px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
-    router.events.subscribe((val) => {
-      console.log(val.url);
-      if (val.url === '/login') { this.hidebtn = true; } else { this.hidebtn = false; }
+    router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        if (val.url === '/login') {
+          this.hidebtn = true; this.sidenav.close(); } else { this.hidebtn = false; }
+      }
     });
   }
 
