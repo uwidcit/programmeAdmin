1d578474317efc193d1540cd7d1038b6c371f61b
Qarun <qarun80@hotmail.com>
2018-07-09 13:28:18 -0400

    test

--- test.js
+++ test.js
@@ -1,17 +1,28 @@
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const allProgs = require('./results.json');
+// const admin = require('firebase-admin');
+// const functions = require('firebase-functions');
+// const allProgs = require('./results.json');
 
-admin.initializeApp(functions.config().firebase);
+// admin.initializeApp(functions.config().firebase);
+
+// var db = admin.firestore();
+// var docRef = db.collection('allProgs').doc('allProgs');
+
+// allProgs.forEach(obj => {
+//     docRef.set(obj);
+// })
+
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
 
 var db = admin.firestore();
+
 var docRef = db.collection('allProgs').doc('allProgs');
 
-allProgs.forEach(obj => {
-    docRef.set(obj);
-})
-
-
-
-
-
+var setAda = docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
