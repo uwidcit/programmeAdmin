86e9e80710685358a0e5dc27c966587d7e44f215
Qarun <qarun80@hotmail.com>
2018-07-16 23:17:44 -0400

    please work :(

--- src/app/errors/errors.component.spec.ts
+++ src/app/errors/errors.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorsComponent } from './errors.component';
+import {SpinnerComponent} from '../shared/spinner.component';
 
 describe('ErrorsComponent', () => {
   let component: ErrorsComponent;
@@ -8,7 +9,7 @@ describe('ErrorsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ErrorsComponent ]
+      declarations: [ ErrorsComponent, SpinnerComponent ]
     })
     .compileComponents();
   }));
