f5f16e8adbbfc80baf010f67de9c5367e5014188
Qarun <qarun80@hotmail.com>
2018-07-23 08:32:37 -0400

    - added comments to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  facURL: 'https://snickdx.me:3004/sta/faculties',
+  facURL: 'https://snickdx.me:3004/sta/faculties', // returns an object contain the faculty abbreviation as the key and the full name as the value
   allProgsBy: 'https://snickdx.me:3004/sta/programmes/',
-  programmes: 'https://snickdx.me:3004/sta/programmes',
-  facStats: 'https://snickdx.me:3004/sta/stats',
-  subjects: 'https://snickdx.me:3004/subjects',
+  facStats: 'https://snickdx.me:3004/sta/stats', // returns an object containing each faculty and the number of programmes offered in each
+  subjects: 'https://snickdx.me:3004/subjects', // returns a list fo all CSEC and CAPE subjects stored in database
   testJSON: '../assets/mock-data/test.json',
-  getErrors: 'https://snickdx.me:3004/errors',
+  getErrors: 'https://snickdx.me:3004/errors', //  returns a JSON array of all subjects failing validation after parsing
+  upload: 'https://snickdx.me:3004/upload', // URL to upload excel file to
   faculties: {
     'Social Sciences': 'fss',
     'Engineering': 'eng',
