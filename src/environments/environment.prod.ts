1d0b954664236073487981e5d02ea59e7ab4f810
Qarun <qarun80@hotmail.com>
2018-07-23 06:27:14 -0400

    - added progress spinner to programme area - changed the name of view page from edit-programme-page.component.html to view.component.html and all associated files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   facURL: 'https://snickdx.me:3004/sta/faculties',
   allProgsBy: 'https://snickdx.me:3004/sta/programmes/',
   programmes: 'https://snickdx.me:3004/sta/programmes',
+  facStats: 'https://snickdx.me:3004/sta/stats',
   subjects: 'https://snickdx.me:3004/subjects',
   testJSON: '../assets/mock-data/test.json',
   getErrors: 'https://snickdx.me:3004/errors',
